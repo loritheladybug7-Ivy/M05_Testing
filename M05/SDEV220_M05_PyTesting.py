b1223d50a3688131d12d79cd588a3cf00b3e4e6c
loritheladybug7-Ivy <lwalden18@ivytech.edu>
2024-07-05 08:35:05 -0400

    Add files via upload

---
+++ M05/SDEV220_M05_PyTesting.py
@@ -0,0 +1,10 @@
+# Lori Walden
+# SDEV220
+# M05 Programming Assignment - Testing
+# This program is for practicing testing in Python
+# https://realpython.com/python-testing/#toc
+
+assert sum([1, 1, 1]) == 6, "Should be 6"
+
+
+
